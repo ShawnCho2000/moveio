d0efe9f8e694c339fdd8ce053332753939f6ae8a
Shawn Cho <shawn2000@outlook.com>
2022-02-26 12:14:27 -0800

    init

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, Response
+import cv2
+import mediapipe as mp
+import time
+
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+
+
+
+
+@app.route("/")
+def homepage():
+    return render_template("page.html", title="HOME PAGE")
+
+@app.route("/docs")
+def docs():
+    return render_template("page.html", title="docs page")
+
+@app.route("/about")
+def about():
+    return render_template("page.html", title="about page")
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen_frames():
+
+    pTime = 0
+    cTime = 0
+    while True:
+        success, img = camera.read()  # read the camera frame
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+        print(results.multi_hand_landmarks)
+
+        if results.multi_hand_landmarks:
+          for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+                # if id == 4:
+                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', img)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+if __name__ == "__main__":
+    app.run(debug=True)
